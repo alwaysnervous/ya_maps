b35f07cc25c39c0e15c43c5434a6c5405f5a7580
mironenkodi <d.i.mironenko@yandex.ru>
2024-02-02 18:32:57 +0300

    функции для кнопок up, down, right, left

--- functions2.py
+++ functions2.py
@@ -11,3 +11,25 @@ def scaling_down(sl):
     get_map(sl.lat, sl.lot, sl.spn)
 
 
+def up(sl):
+    if -7.483765 >= sl.lot >= -47.483765:
+        sl.lat += sl.height
+    get_map(sl.lat, sl.lot, sl.spn)
+
+
+def down(sl):
+    if -7.483765 >= sl.lot >= -47.483765:
+        sl.lat -= sl.height
+    get_map(sl.lat, sl.lot, sl.spn)
+
+
+def right(sl):
+    if 155.746181 >= sl.lat >= 115.746181:
+        sl.lat += sl.width
+    get_map(sl.lat, sl.lot, sl.spn)
+
+
+def left(sl):
+    if 155.746181 >= sl.lat >= 115.746181:
+        sl.lat -= sl.width
+    get_map(sl.lat, sl.lot, sl.spn)
